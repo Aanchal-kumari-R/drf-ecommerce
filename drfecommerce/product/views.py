f34364475b52b0080652600cdd4468665e446d7d
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-15 16:19:44 +0530

    Filter Product By Category

--- drfecommerce/product/views.py
+++ drfecommerce/product/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets 
-from rest_framework.response import Response  
+from rest_framework.response import Response   
+from rest_framework.decorators import action
 from drf_spectacular.utils import extend_schema
 from drfecommerce.product.models import (Brand ,Category, Product)  
 from drfecommerce.product.serializers import (ProductSerializer,BrandSerializer,CategorySerializer)
@@ -15,7 +16,7 @@ class CategoryViewSet(viewsets.ViewSet):
         return Response(serializer.data)  
 
 class BrandViewSet(viewsets.ViewSet):  
-    serializer_class = BrandSerializer 
+    serializer_class = BrandSerializer  
     @extend_schema(request=BrandSerializer)
     def list(self,request): 
         queryset = Brand.objects.all() 
@@ -23,9 +24,20 @@ class BrandViewSet(viewsets.ViewSet):
         return Response(serializer.data) 
     
 class ProductViewSet(viewsets.ViewSet): 
+    queryset = Product.objects.all()  
+    
+    @action(
+            url_path=r"category/(?P<category>[^/]+)/all", 
+            detail=False, 
+            methods=['get'] 
+    )
+    def list_products_by_category(self,request,category=None):  
+        serialized = ProductSerializer(self.queryset.filter(category__name=category),many=True) 
+        return Response(serialized.data) 
+     
+
     serializer_class = ProductSerializer  
     @extend_schema(request=ProductSerializer)
     def list(self,request): 
-        queryset = Product.objects.all()
-        serializer = ProductSerializer(queryset,many=True) 
+        serializer = ProductSerializer(self.queryset,many=True) 
         return Response(serializer.data) 

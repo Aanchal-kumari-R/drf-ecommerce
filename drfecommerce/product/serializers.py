1719afd9f552d5d876438eb8b43a5ef0bea384f4
Aanchal-kumari-R <aanchalkumari096@gmail.com>
2024-09-16 14:49:18 +0530

    Nested Serializer

--- drfecommerce/product/serializers.py
+++ drfecommerce/product/serializers.py
@@ -1,10 +1,6 @@
 from rest_framework import serializers 
 from drfecommerce.product.models import (Brand ,Category, Product ,ProductLine) 
 
-class ProductSerializer(serializers.ModelSerializer): 
-    class Meta: 
-         model = Product 
-         fields = '__all__' 
 
 class ProductLineSerializer(serializers.ModelSerializer): 
     class Meta: 
@@ -16,12 +12,23 @@ class BrandSerializer(serializers.ModelSerializer):
          model = Brand 
          fields = '__all__' 
 
-class CategorySerializer(serializers.ModelSerializer): 
+class CategorySerializer(serializers.ModelSerializer):  
     class Meta: 
          model = Category 
          fields = '__all__' 
 
 
-
-
-    
+class ProductSerializer(serializers.ModelSerializer): 
+    brand_name = serializers.CharField(source = "brand.name") 
+    category_name = serializers.CharField(source = "category.name") 
+    product_line = ProductLineSerializer(many = True) 
+    class Meta: 
+         model = Product 
+         fields = ( 
+              "name",
+              "desc", 
+              "brand_name", 
+              "category_name", 
+              "product_line",
+         ) 
+  
